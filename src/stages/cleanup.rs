27142b7dc820ebeba5de7c176fd1996e7e587871
RodogInfinite <wjroland@gmail.com>
2022-07-16 14:47:27 -0400

    Add comments

--- src/stages/cleanup.rs
+++ src/stages/cleanup.rs
@@ -1,1 +1,2 @@
-
+// Bevy has a built-in cleanup system that will remove all data stored in components that will then be refreshed in the next extract stage. For persistent data over multiple frames, store in resources for control. If resource data must be cleared on occasion, then add custom cleanup code here.
+// https://bevy-cheatbook.github.io/gpu/stages.html#cleanup
--- src/stages/phase_sort.rs
+++ src/stages/phase_sort.rs
@@ -1,1 +1,3 @@
-
+// Sort the PhaseItems from the Queue stage to then pass on to the render stage
+// This stage will rarely need custom code
+// https://bevy-cheatbook.github.io/gpu/stages.html#phasesort
