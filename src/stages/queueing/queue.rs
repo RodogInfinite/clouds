0d3704ab95277f7009e8765ee7325fc2ab175a61
RodogInfinite <wjroland@gmail.com>
2022-08-02 14:41:32 -0400

    Change from Transparent3d to Opaque3d

--- src/stages/queueing/queue.rs
+++ src/stages/queueing/queue.rs
@@ -1,5 +1,5 @@
 use bevy::{
-    core_pipeline::core_3d::Transparent3d,
+    core_pipeline::core_3d::Opaque3d,
     pbr::{MeshPipelineKey, MeshUniform,},
     prelude::*,
     render::{
@@ -18,23 +18,23 @@ use super::{pipeline::CustomPipeline, draw::DrawCustom};
 
 #[allow(clippy::too_many_arguments)]
 pub fn queue_custom(
-    transparent_3d_draw_functions: Res<DrawFunctions<Transparent3d>>,
+    opaque_3d_draw_functions: Res<DrawFunctions<Opaque3d>>,
     custom_pipeline: Res<CustomPipeline>,
     msaa: Res<Msaa>,
     mut pipelines: ResMut<SpecializedMeshPipelines<CustomPipeline>>,
     mut pipeline_cache: ResMut<PipelineCache>,
     meshes: Res<RenderAssets<Mesh>>,
     material_meshes: Query<(Entity, &MeshUniform, &Handle<Mesh>), With<InstanceMaterialData>>,
-    mut views: Query<(&ExtractedView, &mut RenderPhase<Transparent3d>)>,
+    mut views: Query<(&ExtractedView, &mut RenderPhase<Opaque3d>)>,
 ) {
-    let draw_custom = transparent_3d_draw_functions
+    let draw_custom = opaque_3d_draw_functions
         .read()
         .get_id::<DrawCustom>()
         .unwrap();
 
     let msaa_key = MeshPipelineKey::from_msaa_samples(msaa.samples);
 
-    for (view, mut transparent_phase) in &mut views {
+    for (view, mut opaque_phase) in &mut views {
         let rangefinder = view.rangefinder3d();
         for (entity, mesh_uniform, mesh_handle) in &material_meshes {
             if let Some(mesh) = meshes.get(mesh_handle) {
@@ -43,7 +43,7 @@ pub fn queue_custom(
                 let pipeline = pipelines
                     .specialize(&mut pipeline_cache, &custom_pipeline, key, &mesh.layout)
                     .unwrap();
-                transparent_phase.add(Transparent3d {
+                opaque_phase.add(Opaque3d {
                     entity,
                     pipeline,
                     draw_function: draw_custom,
